363e4d0c6fd513bb550cdd982ebf73efb34258ac
Elmar Pitschke <hugoki@php.net>
2006-06-27 12:42:54 +0000

    - Corrected example
    git-svn-id: https://svn.php.net/repository/pear/packages/Structures_BibTex/trunk@215419 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/Structures_BibTex_example.php
+++ examples/Structures_BibTex_example.php
@@ -1,10 +1,3 @@
-<html>
-<head></head>
-<body>
-<h1>Example of Structurs_BibTex</h1>
-<p>This example parses the <a href="./example.bib">BibTex File &quot;example.bib&quot;</a>
-and adds en entry to it. Then the internal stored data is printed and the
-converted BibTex. An here is the <a href="./example.phps">source file of this example</a>.</p>
 <?php
 require_once 'PEAR.php';
 require_once './Structures_BibTex.php';
@@ -19,21 +12,22 @@ if(PEAR::isError($ret)) {
 }
 $foo->parse();
 
-//Adding an entry
+//Creating an entry
 $addarray = array();
-$addarray['type'] = 'Article';
-$addarray['cite'] = 'art2';
-$addarray['title'] = 'Titel2';
-$addarray['author'][0] = 'John Doe';
-$addarray['author'][1] = 'Jane Doe';
+$addarray['type']               = 'Article';
+$addarray['cite']               = 'art2';
+$addarray['title']              = 'Titel2';
+$addarray['author'][0]['first'] = 'John';
+$addarray['author'][0]['last']  = 'Doe';
+$addarray['author'][1]['first'] = 'Jane';
+$addarray['author'][1]['last']  = 'Doe';
+//Adding the entry
 $foo->addEntry($addarray);
 
 //Printing the result
 print "<pre>";
-print "Konverting This Array:\n\n";
+print "Converting This Array:\n\n";
 print_r($foo->data);
 print "\nInto this:\n\n";
 print $foo->bibTex();
 ?>
-</body>
-</html>
