fca1d3cc5d0599d0c60f664e164a6be3242412e0
Daniel O'Connor <clockwerx@php.net>
2012-01-17 14:07:04 +0000

    Bug #14442 Undefined variable  in _removeCurlyBraces
    git-svn-id: https://svn.php.net/repository/pear/packages/Structures_BibTex/trunk@322405 c90b9560-bf6c-de11-be94-00142212c4b1

--- Structures/BibTex.php
+++ Structures/BibTex.php
@@ -850,8 +850,8 @@ class Structures_BibTex
     {
         //First we save the delimiters
         $beginningdels = array_keys($this->_delimiters);
-        $firstchar     = substr($entry, 0, 1);
-        $lastchar      = substr($entry, -1, 1);
+        $firstchar     = substr($value, 0, 1);
+        $lastchar      = substr($value, -1, 1);
         $begin         = '';
         $end           = '';
         while (in_array($firstchar, $beginningdels)) { //The first character is an opening delimiter
